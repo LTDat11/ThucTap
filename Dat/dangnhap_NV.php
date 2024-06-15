0f8dfba01022d083e8d6eb4bc0bec82f51b602d0
thanh <quangdonghathanh@gmail.com>
2024-06-15 18:33:19 +0700

    chỉnh giao diện đki đăng nhập

--- Dat/dangnhap_NV.php
+++ Dat/dangnhap_NV.php
@@ -45,7 +45,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Đăng nhập</title>
 
     <link href="assets/img/vnpt.png" rel="icon">
+    <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
 
+<!-- Vendor CSS Files -->
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <!-- Font Icon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -55,11 +63,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- Main css -->
-    <link rel="stylesheet" href="assets/css/dangnhap.css">
+    <link rel="stylesheet" href="assets/css/dangnhapTest.css">
 </head>
 
 <body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center">
+        <div class="container d-flex justify-content-between">
 
+        <div class="logo">
+            <!-- Uncomment below if you prefer to use an text logo -->
+            <!-- <h1><a href="index.html">NewBiz</a></h1> -->
+            <a href="trangchu.php"><img src="assets/img/logovnpt.png" alt="" class="img-fluid"></a>
+        </div>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="index.php">Trang Chủ</a></li>
+                <li><a class="nav-link scrollto" href="#about">Về Chúng Tôi</a></li>
+                <li><a class="nav-link scrollto" href="#services">Dịch Vụ</a></li>
+                <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+                <li><a class="nav-link scrollto" href="#team">Đội Ngũ</a></li>
+                
+                <li><a class="nav-link scrollto" href="#contact">Liên Hệ</a></li>
+                    <li class="dropdown">
+                        <a href="#"><span>Thành Viên</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="dangky.php">Đăng ký</a></li>
+                            <li><a href="dangnhap.php">Đăng nhập</a></li>
+                        </ul>
+                    </li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- #header -->
     <div class="main">
 
         <!-- Sign in Form -->
@@ -97,6 +136,92 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </section>
 
     </div>
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+            <div class="col-lg-4 col-md-6 footer-info">
+                <h3>VNPT</h3>
+                <p>Với nhiều năm hình thành, phát triển và gắn bó với thị trường viễn thông tại Việt Nam, Tập đoàn Bưu chính
+                Viễn thông VNPT hiện là một trong những nhà cung cấp các dịch vụ internet uy tín hàng đầu.</p>
+            </div>
+
+            <div class="col-lg-2 col-md-6 footer-links">
+                <h4>Liên Kết Hữu Ích</h4>
+                <ul>
+                <li><a href="#">Trang chủ</a></li>
+                <li><a href="#">Về chúng tôi</a></li>
+                <li><a href="#">Dịch vụ</a></li>
+                <li><a href="#">Điều khoản dịch vụ</a></li>
+                <li><a href="#">Chính sách bảo mật</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-contact">
+                <h4>Liên Hệ</h4>
+                <p>
+                11 Phan Đình Phùng, Tân An, Ninh Kiều<br>
+                Cần Thơ 92000<br>
+                Việt Nam <br>
+                <strong>Điện thoại:</strong> 0913 737 475<br>
+                <strong>Email:</strong> vnpt@gmail.com<br>
+                </p>
+
+                <div class="social-links">
+                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                </div>
+
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-newsletter">
+                <h4>Bản Tin Của Chúng Tôi</h4>
+                <p>VNPT là nhà cung cấp mạng uy tín hàng đầu tại Việt Nam với hơn 60 năm kinh nghiệm phát triển, đi đầu về
+                công nghệ, chất lượng dịch vụ và chăm sóc khách hàng. Cáp quang VNPT ứng dụng công nghệ mới nhất trên thế
+                giới là Gpon/AON cho chất lượng đường truyền nhanh và ổn định.</p>
+                <!-- <form action="" method="post">
+                <input type="email" name="email"><input type="submit" value="Đăng Ký">
+                </form> -->
+            </div>
+
+            </div>
+        </div>
+        </div>
+
+        <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong>VNPT</strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!--
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=NewBiz
+        -->
+            <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+        </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
 
     <!-- JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
